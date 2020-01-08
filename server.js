6946d56c2511a502404c5536f1d38ac7c78bd4f7
vega <stanislasvega@yahoo.fr>
2020-01-08 09:28:41 +0100

    change listening on 5000

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.post('/api/v1/partners', async (req, res) => {
 });
 
 
-app.listen(3000,() => {
-  console.log('Listening on port 3000')
+app.listen(5000,() => {
+  console.log('Listening on port 5000')
 })
