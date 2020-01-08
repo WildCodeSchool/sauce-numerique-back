368ebb6fd5dcf26bb4c82a03f560066bb379dff6
vega <stanislasvega@yahoo.fr>
2020-01-08 13:35:05 +0100

    add put and delete route

--- server.js
+++ server.js
@@ -7,18 +7,37 @@ const app = express();
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+app.get('/api/v1/partners/:id', async(req,res) => {
+  const { id } = req.params;
+  const onePartners = await Partners.findOne({where: { id }})
+  res.send(onePartners)
+})
+
 app.get('/api/v1/partners', async(req, res) => {
-    const allPartners = await Partners.findAll()
-    res.send(allPartners) 
+    const AllPartners = await Partners.findAll()
+    res.send(AllPartners) 
     });
 
 
 app.post('/api/v1/partners', async (req, res) => {
   const { name, logo } = req.body
-  const newPartners =await Partners.create({name, logo})
-  res.send(newPartners);
+  const NewPartners =await Partners.create({name, logo})
+  res.send(NewPartners);
 });
 
+app.put('/api/v1/partners/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, logo} = req.body;
+  const UpdatePartners = await Partners.update({ name, logo }, {where: { id }});
+  res.send(UpdatePartners);
+
+})
+
+app.delete('/api/v1/partners/:id', async (req, res) => {
+  const { id } = req.params;
+  await Partners.destroy({ where: { id }});
+  res.send(id);
+})
 
 app.listen(5000,() => {
   console.log('Listening on port 5000')
