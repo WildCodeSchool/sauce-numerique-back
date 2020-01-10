e08969f5c810b608d0d944ce01ce2fca03ad109d
Nicolas Brondin-Bernard <nicolas@brondin.com>
2020-01-10 10:36:35 +0100

    [HOST] server - typo on port

--- server.js
+++ server.js
@@ -12,6 +12,6 @@ app.use("/api/v1/ressources", require('./routes/ressources'));
 
 
 
-app.listen(process.env.port || 5000,() => {
-  console.log('Listening on port 5000')
+app.listen(process.env.PORT || 5000,() => {
+  console.log('Listening on port '+ process.env.PORT || 5000);
 })
