307c042e697290d560b6b675d25b986510f89c28
vega <stanislasvega@yahoo.fr>
2020-01-14 17:22:47 +0100

    remove port for mysql

--- config/config.js
+++ config/config.js
@@ -5,7 +5,6 @@ module.exports =
 {
   "development": {
     "username": process.env.MYSQL_USER,
-    "port": process.env.MYSQL_PORT,
     "password": process.env.MYSQL_PASSWORD,
     "database": process.env.MYSQL_DATABASE,
     "host": process.env.MYSQL_HOST,
